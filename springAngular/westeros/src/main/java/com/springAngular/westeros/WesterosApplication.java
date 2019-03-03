eeed5b284e2251ce2bdb286e7401300eb84a7f0e
felipefrocha <felipefonsecarocha@gmail.com>
2019-03-03 19:34:50 -0300

    initial commit

---
+++ springAngular/westeros/src/main/java/com/springAngular/westeros/WesterosApplication.java
@@ -0,0 +1,13 @@
+package com.springAngular.westeros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesterosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WesterosApplication.class, args);
+	}
+
+}
---
+++ springAngular/westeros/src/test/java/com/springAngular/westeros/WesterosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springAngular.westeros;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WesterosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
