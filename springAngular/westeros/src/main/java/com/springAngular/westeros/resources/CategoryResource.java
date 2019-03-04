49f71a49cae6b917a79cfdcaf97e122ec836ab16
felipefrocha <felipefonsecarocha@gmail.com>
2019-03-04 10:57:57 -0300

    category

--- springAngular/westeros/src/main/java/com/springAngular/westeros/resources/CategoryResource.java
+++ springAngular/westeros/src/main/java/com/springAngular/westeros/resources/CategoryResource.java
@@ -1,17 +1,34 @@
 package com.springAngular.westeros.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springAngular.westeros.domain.Category;
+
 @RestController
-@RequestMapping(value="/graphs")
-public class GraphResource {
+@RequestMapping(value="/category")
+public class CategoryResource {
 	
-	
+
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest esta funcionando!";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1,"Graph");
+		Category cat2 = new Category(2,"Node");
+		Category cat3 = new Category(3,"Edge"); 
+		
+		List<Category> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+		list.add(cat3);
+		
+		
+		return list;
 	}
 
 }
